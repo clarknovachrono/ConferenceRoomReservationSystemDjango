b8bc2f02beee0caae8e4a962d7a70c711aae166b
v-clarkcodes <vinceclarkt@gmail.com>
2022-03-24 13:19:26 +0800

    8th commit

--- conferenceroomreservation/settings.py
+++ conferenceroomreservation/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'conferenceroomreservation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if (DEBUG is True):
+# if (DEBUG is True): turn on if local
 #     DATABASES = {
 #         'default': {
 #             # 'ENGINE': 'django.db.backends.sqlite3',
